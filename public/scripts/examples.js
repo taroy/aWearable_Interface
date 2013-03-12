883992ca2ad9bc0fb7c8c5103b118ae5d138632d
Maria Moeller <mariamollr@gmail.com>
2013-03-12 09:28:19 +0100

    progression tracker

--- public/scripts/examples.js
+++ public/scripts/examples.js
@@ -78,5 +78,12 @@ define(function() {
     
   };
   
+  $(window).load(function(){
+    $("ol.progtrckr").each(function(){
+        $(this).attr("data-progtrckr-steps", 
+                     $(this).children("li").length);
+    });
+  })
+  
   return Events;
 });
